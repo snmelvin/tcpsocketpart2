521d1512dbd20a9e441ba0f2d2c3ba874cc514fb
snmelvin <snmelvin@email.wm.edu>
2017-04-20 18:42:40 -0400

    initial commit

---
+++ TCPTimer.java
@@ -0,0 +1,26 @@
+
+import java.util.TimerTask;
+import java.util.Timer;
+
+public class TCPTimer extends TimerTask {
+
+    private BaseSocketImpl sock;
+    private Object ref;
+
+    /**
+     * register timer event for TCP statck
+     * @param tcpTimer TImer object to use
+     * @param delay length of time before timer in milliseconds
+     * @param sock socket implementation to call sock.handleTimer(ref)
+     * @param ref generic object of information to pass back
+     */
+    public TCPTimer(Timer tcpTimer, long delay, BaseSocketImpl sock, Object ref){
+        this.sock = sock;
+        this.ref = ref;
+        tcpTimer.schedule(this, delay);
+    }
+
+    public void run(){
+        sock.handleTimer(ref);
+    }
+}
